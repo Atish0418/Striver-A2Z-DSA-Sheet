cd7e072e017d90f4f918376fdcd748a7f4b2dfaa
Atish Adhav <adhavatish04@gmail.com>
2025-04-18 21:57:19 +0530

    Added Optimal approch

--- Array/Easy/Java/MoveZero.java
+++ Array/Easy/Java/MoveZero.java
@@ -1,30 +1,59 @@
-import java.util.*;
+// import java.util.*;
 
-public class MoveZero {
+// public class MoveZero {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+
+//         int[] temp = new int[arr.length];
+//         int index = 0;
+
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] != 0) {
+//                 temp[index] = arr[i];
+//                 index++;
+//             }
+//         }
+
+//         for (int i = 0; i < index; i++) {
+//             arr[i] = temp[i];
+//         }
+
+//         int nz = index;
+//         for (int i = nz; i < arr.length; i++) {
+//             arr[i] = 0;
+//         }
+
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+// Optimal Approach
+
+public class MoveZero{
     public static void main(String[] args) {
-        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int arr[] = {1,0,2,3,2,0,0,4,5,1};
 
-        int[] temp = new int[arr.length];
-        int index = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                temp[index] = arr[i];
-                index++;
+        int j = -1;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == 0){
+                 j = i;
+                 break;
             }
         }
 
-        for (int i = 0; i < index; i++) {
-            arr[i] = temp[i];
+        for(int i=j+1; i<arr.length; i++){
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
         }
 
-        int nz = index;
-        for (int i = nz; i < arr.length; i++) {
-            arr[i] = 0;
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
         }
     }
 }
