3f3d1e8dadc9436c03972dd01f06c352bd6d35e8
Atish Adhav <adhavatish04@gmail.com>
2025-04-18 22:22:19 +0530

    Added Optimal-MovZero

--- Array/Easy/JavaScript/MoveZero.js
+++ Array/Easy/JavaScript/MoveZero.js
@@ -1,25 +1,50 @@
 let prompt = require("prompt-sync")()
 let arr = [1,0,2,3,2,0,0,4,5,1];
 
-let temp = new Array(arr.length);
-let index = 0;
+// let temp = new Array(arr.length);
+// let index = 0;
 
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i] !== 0){
+//         temp[index] = arr[i];
+//         index++;
+//     }
+// }
+
+// for(let i=0; i<index; i++){
+//     arr[i] = temp[i];
+// }
+
+// let nz = index;
+// for(let i=nz; i<arr.length; i++){
+//     arr[i] = 0;
+// }
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+let j = -1;
 for(let i=0; i<arr.length; i++){
-    if(arr[i] !== 0){
-        temp[index] = arr[i];
-        index++;
+    if(arr[i] === 0){
+        j = i;
+        break;
     }
 }
 
-for(let i=0; i<index; i++){
-    arr[i] = temp[i];
+if(j == -1){
+    return;
 }
 
-let nz = index;
-for(let i=nz; i<arr.length; i++){
-    arr[i] = 0;
+for(let i = j+1; i<arr.length; i++){
+    if(arr[i] !== 0){
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++;
+    }
 }
 
 for(let i=0; i<arr.length; i++){
-    console.log(arr[i]);
+    process.stdout.write(`${arr[i]} `);
 }
